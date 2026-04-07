463e549272e1522bc2cc99d70a51d81f954e039e
Jawaher <8alsaadii@gmail.com>
2026-04-07 11:29:36 +0400

    solve task2: caughtSpeeding

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     static void main(String[] args) {
-        System.out.println("Task 1:" + cigarParty(50,false));
-
+        //task 1
+        System.out.println("Task 1:" + cigarParty(50, false));
+        //task 2
+        System.out.println("Task 2:" + caughtSpeeding(65, true));
     }
 
-    //Task :  cigarParty
-    public static boolean cigarParty (int cigars, boolean isWeekend) {
+    //Task 1:  cigarParty
+    public static boolean cigarParty(int cigars, boolean isWeekend) {
         if (isWeekend) return cigars >= 40;
-        return (cigars >= 40 && cigars <=60);
+        return (cigars >= 40 && cigars <= 60);
+    }
+
+    //Task 2: caughtSpeeding
+    public static int caughtSpeeding(int speed, boolean isBirthday) {
+        int limit1 = 60;
+        int limit2 = 80;
+        if (isBirthday) {
+            limit1 = 65;
+            limit2 = 85;
+        }
+        if (speed <= limit1) {
+            return 0;
+        }
+        if (speed <= limit2) {
+            return 1;
+        }
+        return 2;
     }
 }
